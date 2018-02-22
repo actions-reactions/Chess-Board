28f64de30097a272c5520b1e1847e7c6238f9226
PriyoMukul <priyomukul@gmail.com>
2018-02-23 00:10:17 +0600

    Color Code print in console statement is proudly added. :boom: :v:

--- main.c
+++ main.c
@@ -13,13 +13,13 @@
 #include <stdio.h>
 
 void display(void);
-void sb(int, int);
+void sb(int, int, int);
 void draw(int);
 void init(void);
 
 
 // Single square block
-void sb(int x, int y) {
+void sb(int x, int y, int c) {
     glBegin(GL_POLYGON);
     glVertex2d(x + 0, y + 0);
     glVertex2d(x + 100, y + 0);
@@ -27,28 +27,29 @@ void sb(int x, int y) {
     glVertex2d(x + 0, y + 100);
     glEnd();
     
+    
     printf("// #Block:\n\n");
+    ( c == 1 ) ? printf("glColor3ub(255, 255, 255);\n") : printf("glColor3ub(0, 0, 0);\n");
     printf("glBegin(GL_POLYGON);\n");
     printf("glVertex2d(%d, %d);\n", x, y);
     printf("glVertex2d(%d, %d);\n", x + 100, y);
     printf("glVertex2d(%d, %d);\n", x + 100, y + 100);
     printf("glVertex2d(%d, %d);\n", x, y + 100);
     printf("glEnd();\n\n");
+    
 }
 
 //Draws 2 lines of blocks starting with white and black
 void draw(int z) {
-    int i, x1 = 0, y1 = 0, x2 = 0, y2 = 0, ay = 0;
+    int i, x1 = 0, y1 = 0, x2 = 0, y2 = 0;
     
     for (i = 0; i < 8; i++) {
         if (i % 2 == 0) {
             glColor3ub(255, 255, 255);
-            ay = y1 + (z-1) * 100;
-            sb(x1, ay);
+            sb(x1, y1 + (z-1) * 100, 1);
         } else {
             glColor3ub(0, 0, 0);
-            ay = y1 + (z-1) * 100;
-            sb(x1, ay);
+            sb(x1, y1 + (z-1) * 100, 0);
         }
         x1 = x1 + 100;
     }
@@ -56,12 +57,10 @@ void draw(int z) {
     for (i = 0; i < 8; i++) {
         if (i % 2 == 0) {
             glColor3ub(0, 0, 0);
-            sb(x2, y2 + 100 + (z-1) * 100);
+            sb(x2, y2 + 100 + (z-1) * 100, 0);
         } else {
             glColor3ub(255, 255, 255);
-
-            sb(x2, y2 + 100 + (z-1) * 100);
-
+            sb(x2, y2 + 100 + (z-1) * 100, 1);
         }
         x2 = x2 + 100;
     }
@@ -74,11 +73,17 @@ void display(void) {
     /* draw white polygon (rectangle) with corners at
      * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
      */
+    
+    
     draw(1);    //For line 1-2
     draw(3);    //For line 3-4
     draw(5);    //For line 5-6
     draw(7);    //For line 7-8
-
+    
+    
+    
+    
+    
     /* don't wait!
      * start processing buffered OpenGL routines
      */
