39385d00ff02c054a77d189e34b4e18859aadb00
PriyoMukul <priyomukul@gmail.com>
2018-02-22 23:24:20 +0600

    Proudly Done by Priyo Mukul the Boss :boom: :poop: :v:

--- main.c
+++ main.c
@@ -1,72 +1,83 @@
+//
+//  main.cpp
+//  assignment-2
+//
+//  Created by Priyo Mukul on 22/2/18.
+//  Copyright © 2018 Priyo Mukul. All rights reserved.
+//
+
 #include <GL/gl.h>
 #include <GL/glut.h>
+
+// #include <GLUT/GLUT.h>
+#include <stdio.h>
+
+void display(void);
+void sb(int, int);
+void draw(int);
+void init(void);
+
+
+// Single square block
+void sb(int x, int y) {
+    glBegin(GL_POLYGON);
+    glVertex2d(x + 0, y + 0);
+    glVertex2d(x + 100, y + 0);
+    glVertex2d(x + 100, y + 100);
+    glVertex2d(x + 0, y + 100);
+    glEnd();
+    
+    printf("// #Block:\n\n");
+    printf("glBegin(GL_POLYGON);\n");
+    printf("glVertex2d(%d, %d);\n", x, y);
+    printf("glVertex2d(%d, %d);\n", x + 100, y);
+    printf("glVertex2d(%d, %d);\n", x + 100, y + 100);
+    printf("glVertex2d(%d, %d);\n", x, y + 100);
+    printf("glEnd();\n\n");
+}
+
+//Draws 2 lines of blocks starting with white and black
+void draw(int z) {
+    int i, x1 = 0, y1 = 0, x2 = 0, y2 = 0, ay = 0;
+    
+    for (i = 0; i < 8; i++) {
+        if (i % 2 == 0) {
+            glColor3ub(255, 255, 255);
+            ay = y1 + (z-1) * 100;
+            sb(x1, ay);
+        } else {
+            glColor3ub(0, 0, 0);
+            ay = y1 + (z-1) * 100;
+            sb(x1, ay);
+        }
+        x1 = x1 + 100;
+    }
+    
+    for (i = 0; i < 8; i++) {
+        if (i % 2 == 0) {
+            glColor3ub(0, 0, 0);
+            sb(x2, y2 + 100 + (z-1) * 100);
+        } else {
+            glColor3ub(255, 255, 255);
+
+            sb(x2, y2 + 100 + (z-1) * 100);
+
+        }
+        x2 = x2 + 100;
+    }
+}
 
 void display(void) {
     /* clear all pixels */
     glClear(GL_COLOR_BUFFER_BIT);
-
+    
     /* draw white polygon (rectangle) with corners at
      * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
      */
-
-    // Single square block
-    void sb(int x, int y) {
-
-        glBegin(GL_POLYGON);
-        glVertex2d(x + 0, y + 0);
-        glVertex2d(x + 100, y + 0);
-        glVertex2d(x + 100, y + 100);
-        glVertex2d(x + 0, y + 100);
-
-
-        glEnd();
-
-    }
-
-
-    //Draws 2 lines of blocks starting with white and black
-    void draw(int z) {
-        int i, x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-
-        for (i = 0; i < 8; i++) {
-            if (i % 2 == 0) {
-                glColor3ub(255, 255, 255);
-                sb(x1, y1 + (z-1) * 100);
-            } else {
-                glColor3ub(0, 0, 0);
-
-                sb(x1, y1 + (z-1) * 100);
-
-            }
-
-            x1 = x1 + 100;
-        }
-
-
-
-        for (i = 0; i < 8; i++) {
-            if (i % 2 == 0) {
-                glColor3ub(0, 0, 0);
-                sb(x2, y2 + 100 + (z-1) * 100);
-            } else {
-                glColor3ub(255, 255, 255);
-
-                sb(x2, y2 + 100 + (z-1) * 100);
-
-            }
-
-            x2 = x2 + 100;
-        }
-    }
-
     draw(1);    //For line 1-2
     draw(3);    //For line 3-4
     draw(5);    //For line 5-6
     draw(7);    //For line 7-8
-
-
-
-
 
     /* don't wait!
      * start processing buffered OpenGL routines
@@ -101,4 +112,3 @@ int main(int argc, char** argv) {
     glutMainLoop();
     return 0; /* ISO C requires main to return int. */
 }
-
