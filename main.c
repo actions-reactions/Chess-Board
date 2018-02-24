67b309b91f06e9f24c4ad0bd081724b10a43aa65
PriyoMukul <priyomukul@gmail.com>
2018-02-24 22:48:00 +0600

    Proudly done by a nested loop! :boom: :v:

--- main.c
+++ main.c
@@ -1,95 +1,60 @@
-#include <GL/gl.h>
-#include <GL/glut.h>
-// #include <GLUT/GLUT.h>
-#include <stdio.h>
+//
+//  main.cpp
+//  ChessBoard
+//
+//  Created by Priyo Mukul on 24/2/18.
+//  Copyright © 2018 Priyo Mukul. All rights reserved.
+//
+
+// for mac os
+#include <GLUT/GLUT.h>
+
+// for windows
+//#include <GL/gl.h>
+//#include <GL/glut.h>
 
 void display(void);
-void sb(int, int, int);
-void draw(int);
 void init(void);
+void boardBlock(int, int, int);
 
-
-// Single square block
-void sb(int x, int y, int c) {
+void boardBlock(int x, int y, int c){
+    ( c == 1 ) ? glColor3ub(255, 255, 255) : glColor3ub(0, 0, 0);
     glBegin(GL_POLYGON);
-    glVertex2d(x + 0, y + 0);
-    glVertex2d(x + 100, y + 0);
-    glVertex2d(x + 100, y + 100);
-    glVertex2d(x + 0, y + 100);
+    glVertex2d(x, y);
+    glVertex2d(x + 150, y);
+    glVertex2d(x + 150, y - 150);
+    glVertex2d(x, y - 150);
     glEnd();
-
-
-    printf("// #Block:\n\n");
-    ( c == 1 ) ? printf("glColor3ub(255, 255, 255);\n") : printf("glColor3ub(0, 0, 0);\n");
-    printf("glBegin(GL_POLYGON);\n");
-    printf("glVertex2d(%d, %d);\n", x, y);
-    printf("glVertex2d(%d, %d);\n", x + 100, y);
-    printf("glVertex2d(%d, %d);\n", x + 100, y + 100);
-    printf("glVertex2d(%d, %d);\n", x, y + 100);
-    printf("glEnd();\n\n");
-
-}
-
-//Draws 2 lines of blocks starting with white and black
-void draw(int z) {
-    int i, x1 = -400, y1 = -400, x2 = -400, y2 = -400;
-
-    for (i = 0; i < 8; i++) {
-        if (i % 2 == 0) {
-            glColor3ub(255, 255, 255);
-            sb(x1, y1 + (z-1) * 100, 1);
-        } else {
-            glColor3ub(0, 0, 0);
-            sb(x1, y1 + (z-1) * 100, 0);
-        }
-        x1 = x1 + 100;
-    }
-
-    for (i = 0; i < 8; i++) {
-        if (i % 2 == 0) {
-            glColor3ub(0, 0, 0);
-            sb(x2, y2 + 100 + (z-1) * 100, 0);
-        } else {
-            glColor3ub(255, 255, 255);
-            sb(x2, y2 + 100 + (z-1) * 100, 1);
-        }
-        x2 = x2 + 100;
-    }
 }
 
 void display(void) {
     /* clear all pixels */
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear (GL_COLOR_BUFFER_BIT);
+    int x = 0, y = 1200, c = 1;
+    for(int i = 1; i <= 8; i++) {
+        for(int j = 1; j <= 8; j++) {
+            boardBlock(x, y, c);
+            x += 150;
+            ( c == 1 ) ? c = 0 : c = 1;
+        }
+        x = 0;
+        ( c == 1 ) ? c = 0 : c = 1;
+        y = y - 150;
+    }
 
-    /* draw white polygon (rectangle) with corners at
-     * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
-     */
-
-
-    draw(1);    //For line 1-2
-    draw(3);    //For line 3-4
-    draw(5);    //For line 5-6
-    draw(7);    //For line 7-8
-
-
-
-
-
-    /* don't wait!
-     * start processing buffered OpenGL routines
-     */
-    glFlush();
+    
+    
+    
+    glFlush ();
 }
-
-void init(void) {
+void init (void) {
     /* select clearing (background) color */
-    glClearColor(0.25, 0.15, 0.0, 0.0);
+    glClearColor (0.0, 0.0, 0.0, 0.0);
     /* initialize viewing values */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(-400, 400, -400, 400);
+    gluOrtho2D(0, 1200, 0, 1200);
 }
-
 /*
  * Declare initial window size, position, and display mode
  * (single buffer and RGBA). Open window with "hello"
@@ -99,11 +64,11 @@ void init(void) {
  */
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(800, 800);
-    glutInitWindowPosition(-400, -400);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (1200, 1200);
+    glutInitWindowPosition (100, 100);
     glutCreateWindow("Chess Board");
-    init();
+    init ();
     glutDisplayFunc(display);
     glutMainLoop();
     return 0; /* ISO C requires main to return int. */
